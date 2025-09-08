8bb748e3c1a9165e7ad0d51592521c529e6bdbcb
Maite Lobo <maitemlobo@gmail.com>
2025-09-08 19:40:25 -0300

    adding .env en root, gitignore creation (#5)
    * correción issues1
    
    * correción issues2
    
    * eliminación de archivos
    
    * creación gitignore

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // server.js
 const express = require('express');
-const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
 const db = require('./db');
 
 const app = express();
-const port = 3000;
+const port = process.env.HTTP_PORT;
 
 // Middleware para procesar los cuerpos de las peticiones en formato JSON
-app.use(bodyParser.json());
+app.use(express.json());
 
 // --- Rutas ---
 
